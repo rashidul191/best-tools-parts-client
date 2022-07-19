fa1e84b38dedfc7d78db7e72fe6d3e043526234a
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-07-19 09:35:09 +0600

    not found page done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
+import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
+    <div>
+      
+
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+
+      </Routes>
      
     </div>
   );
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div>
-            <p>Error! Sorry page node found</p>
-            <h2>404</h2>
-            <p>Back to Home</p>
-        </div>
-    );
+  return (
+    <div className="sm:mt-20 md:mt-52 flex justify-center text-center">
+      <div>
+        <h5 className="text-xl text-error">Error !!! Sorry page not found</h5>
+        <h1 className="text-6xl font-bold text-error my-3">404</h1>
+        <Link to="/" className="text-primary underline">
+          {" "}
+          <p>Back to Home</p>{" "}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
